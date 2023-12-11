7467e256f8e172b27b8a5243f8067a7a5bb58870
Kimra0467 <87680279+Kimra0467@users.noreply.github.com>
2023-12-11 12:05:54 +0900

    Add files via upload

---
+++ week15/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform PlayerPosition;
+    Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - PlayerPosition.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = PlayerPosition.position + offset;
+    }
+}
